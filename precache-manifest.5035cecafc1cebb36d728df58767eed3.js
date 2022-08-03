b60226e387954905291ceb072da7716bdf0d9908
Neha-tft <mishra.neha@tftus.com>
2022-08-03 13:24:40 +0530

    Updates

---
+++ precache-manifest.5035cecafc1cebb36d728df58767eed3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "79b0a9399535c0a69c1c",
+    "url": "/applause-main/static/css/main.bcf83608.chunk.css"
+  },
+  {
+    "revision": "79b0a9399535c0a69c1c",
+    "url": "/applause-main/static/js/main.79b0a939.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/applause-main/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "69fdfdef10bd657551fe",
+    "url": "/applause-main/static/css/2.395dddaf.chunk.css"
+  },
+  {
+    "revision": "69fdfdef10bd657551fe",
+    "url": "/applause-main/static/js/2.69fdfdef.chunk.js"
+  },
+  {
+    "revision": "5a38c73996b269b0befa",
+    "url": "/applause-main/static/js/runtime~main.5a38c739.js"
+  },
+  {
+    "revision": "a187cddbe05209f012b7a8bfc5f572f0",
+    "url": "/applause-main/static/media/del.a187cddb.png"
+  },
+  {
+    "revision": "134f40a294a85e116a148e0f0d606273",
+    "url": "/applause-main/static/media/clap.134f40a2.png"
+  },
+  {
+    "revision": "bd00e2cb0a833216321cae5f40ef91f7",
+    "url": "/applause-main/static/media/bg.bd00e2cb.avif"
+  },
+  {
+    "revision": "39b891221fce8b3c3ff466d25eaf159a",
+    "url": "/applause-main/static/media/anime.39b89122.jfif"
+  },
+  {
+    "revision": "4fee25de4d61abc8d922b872ce39a819",
+    "url": "/applause-main/static/media/concert.4fee25de.jfif"
+  },
+  {
+    "revision": "5d08b6bf9a1baf26f17caf38811e29ed",
+    "url": "/applause-main/static/media/applause1.5d08b6bf.png"
+  },
+  {
+    "revision": "f8788d2ae657ce166ab969f3bca93b3e",
+    "url": "/applause-main/index.html"
+  }
+];
